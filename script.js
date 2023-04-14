3386a9c74fdde1cbc3a067df30a0e56a794571b0
shreesty <shristi35dahal@gmail.com>
2023-04-14 21:04:42 +0545

    Adding blogs

--- script.js
+++ script.js
@@ -25,4 +25,31 @@ contacts.addEventListener('click',function()
 
 })
 
+let firstBlog=document.querySelector('.first-blog');
+
+    let text1 = "10 Tips For Planning A Successful Road Trip";
+    let result1 = text1.link("blog1.html");
+    document.querySelector('.first-blog').innerHTML = result1;
+
+
+let secondBlog=document.querySelector('.second-blog');
+
+    let text2 = "Social media bringing you down?";
+    let result2 = text2.link("blog2.html");
+    secondBlog=document.querySelector('.second-blog').innerHTML = result2;
+
+
+
+let thirdBlog=document.querySelector('.third-blog');
+    let text3 = "What Is The Impact Of Artificial Intelligence (AI) On Society?";
+    let result3 = text3.link("blog3.html");
+    thirdBlog=document.querySelector('.third-blog').innerHTML = result3;
+
+
+
+let fourthBlog=document.querySelector('.fourth-blog');
+
+    let text4 = "The Importance of Self-Care";
+    let result4 = text4.link("blog4.html");
+    fourthBlog=document.querySelector('.fourth-blog').innerHTML = result4;
 

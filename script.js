c9e25fe68c82af17a0c28fd4e19c279a8285ad27
shreesty <shristi35dahal@gmail.com>
2023-04-14 16:00:20 +0545

    change in nav-bar

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let about=document.querySelector('#about');
+let About=document.querySelector('.About');
+// let Blogs=documents.querySelector('.Blogs');
+
+about.addEventListener('click',function()
+{
+
+    let aboutText=window.location.href="about.html";
+      
+})
+
+let authors=document.querySelector('#authors');
+authors.addEventListener('click',function()
+{
+    window.location.href="authors.html";
+
+})
+
+
+let contacts=document.querySelector('#contact');
+contacts.addEventListener('click',function()
+{
+    window.location.href="contact.html";
+
+})
+
+

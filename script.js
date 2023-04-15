531eec68ae7f7b8b790e0031a4a5b530d3114a46
shreesty <shristi35dahal@gmail.com>
2023-04-15 14:41:41 +0545

    change in js

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ contacts.addEventListener('click',function()
     window.location.href="contact.html";
 })
 
+
 let firstBlog=document.querySelector('.first-blog');
 let secondBlog=document.querySelector('.second-blog');
 let thirdBlog=document.querySelector('.third-blog');
@@ -29,8 +30,8 @@ let fourthBlog=document.querySelector('.fourth-blog');
 
 firstBlog.addEventListener("click", function() {
 	window.location.href = firstBlog.textContent;
+    
 });
-
 secondBlog.addEventListener("click", function() {
 	window.location.href = secondBlog.textContent;
 });
@@ -43,3 +44,6 @@ thirdBlog.addEventListener("click", function() {
 fourthBlog.addEventListener("click", function() {
 	window.location.href = fourthBlog.textContent;
 });
+
+
+

8dcfcdcb593ecfaaab8ee222957e91c5c627f80d
shreesty <shristi35dahal@gmail.com>
2023-04-16 08:15:21 +0545

    change in nav-bar

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 
-
 let About = document.querySelector('.About');
 let about = document.querySelector('.about');
-
-// about.addEventListener('click',function()
-// {
-//     let aboutText=window.location.href="about.html";  
-// })
-
-// let authors=document.querySelector('#authors');
-// authors.addEventListener('click',function()
-// {
-//     window.location.href="authors.html";
-// })
-
-// let contacts=document.querySelector('#contact');
-// contacts.addEventListener('click',function()
-// {
-//     window.location.href="contact.html";
-// })
-
-
 let firstBlog = document.querySelector('.first-blog');
 let secondBlog = document.querySelector('.second-blog');
 let thirdBlog = document.querySelector('.third-blog');
@@ -39,7 +19,6 @@ thirdBlog.addEventListener("click", function () {
 	window.location.href = thirdBlog.textContent;
 });
 
-
 fourthBlog.addEventListener("click", function () {
 	window.location.href = fourthBlog.textContent;
 });
@@ -48,3 +27,43 @@ fourthBlog.addEventListener("click", function () {
 
 
 
+
+
+
+
+// import React, { useState } from 'react';
+
+// function SignInForm() {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(`Submitting email ${email} and password ${password}`);
+//     // TODO: Implement form submission logic
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="email">Email:</label>
+//       <input 
+//         type="email" 
+//         id="email" 
+//         value={email} 
+//         onChange={(event) => setEmail(event.target.value)} 
+//         required 
+//       />
+
+//       <label htmlFor="password">Password:</label>
+//       <input 
+//         type="password" 
+//         id="password" 
+//         value={password} 
+//         onChange={(event) => setPassword(event.target.value)} 
+//         required 
+//       />
+
+//       <button type="submit">Sign In</button>
+//     </form>
+//   );
+// }
